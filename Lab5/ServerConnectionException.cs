0b3a338c49a4b7bd2b11089229ccc368c25116bb
GhostInn <112103443+GhostInn@users.noreply.github.com>
2024-06-01 00:01:05 +0300

    Update ServerConnectionException.cs

--- Lab5/ServerConnectionException.cs
+++ Lab5/ServerConnectionException.cs
@@ -2,8 +2,8 @@ namespace Lab5;
 
 public class ServerConnectionException : Exception
 {
-    // Этот конструктор вызывает конструктор базового класса с указанным сообщением об ошибке.
     public ServerConnectionException(string message) : base(message)
     {
+    
     }
 }
