8c1a3781de54683458caef2afbfdef1691534a35
GhostInn <112103443+GhostInn@users.noreply.github.com>
2024-05-31 23:33:58 +0300

    Update AutoUpdate.cs

--- Lab5/AutoUpdate.cs
+++ Lab5/AutoUpdate.cs
@@ -2,18 +2,14 @@ namespace Lab5;
 
 public class AutoUpdate
 {
-    // Этот метод называется "GetNewData" и принимает логический параметр "isConnected".
     public void GetNewData(bool isConnected)
     {
-        // Это оператор if-else, который проверяет значение "isConnected".
         if (isConnected)
         {
-            // Если "isConnected" равно true, то в консоль выводится сообщение "Данные получены".
             Console.WriteLine("Данные получены");
         }
         else
         {
-            // Если "isConnected" равно false, то генерируется исключение "ServerConnectionException" с сообщением "Обрыв связи с сервером".
             throw new ServerConnectionException("Обрыв связи с сервером");
         }
     }
